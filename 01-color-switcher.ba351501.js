bf0f6bbf4b4310105a767b2fcc98032bfdb370b9
AndreeaSichitiu <123585629+AndreeaSichitiu@users.noreply.github.com>
2023-06-23 13:56:59 +0000

    Deploying to gh-pages from @ AndreeaSichitiu/goit-js-hw-09@13c7859696e4f7541109e4e55776b34dce31a5d4 🚀

--- 01-color-switcher.ba351501.js
+++
@@ -1,2 +0,0 @@
-!function(){"use strict";var t=document.querySelector("button  [data-start]"),e=document.querySelector("button[data-stop]");e.disabled=!0,t.addEventListener("click",d),e.addEventListener("click",(function(){clearInterval(a),e.disabled=!0,t.disabled=!1}));var a=setInterval(d,1e3);function d(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0)),t.disabled=!0,e.disabled=!1}}();
-//# sourceMappingURL=01-color-switcher.ba351501.js.map
